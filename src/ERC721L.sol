065fa0ee07085017ba6d909e09a67edbef1a879a
Isaac Patka <ipatka@gmail.com>
2022-07-06 18:05:37 +0200

    wip

---
+++ src/ERC721L.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+contract ERC721L is ERC721 {
+    event CommercialExploitation(uint256 tokenID, string uri);
+
+    function getLicenseId(uint256 tokenId)
+        external
+        view
+        virtual
+        returns (uint256)
+    {}
+
+    function licenseURI(uint256 tokenId)
+        external
+        view
+        virtual
+        returns (string memory)
+    {}
+
+    function copyrightOwner(uint256 tokenId)
+        external
+        virutal
+        returns (address)
+    {}
+
+    function requestReproductionToken(uint256 tokenId)
+        external
+        virtual
+        returns (bool)
+    {}
+
+    function requestDerivativeToken(uint256 tokenId)
+        external
+        virtual
+        returns (bool)
+    {}
+
+    function logCommercialExploitation(uint256 tokenId, string calldata uri)
+        external
+        returns (bool)
+    {
+        emit CommercialExploitation(tokenId, uri);
+    }
+}
